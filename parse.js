6e42a45e9a719657c7d4ed3425961b7a6eee7cef
AndrewBeletskiy <AndrewBeletskiy@gmail.com>
2017-07-06 13:49:26 +0300

    It can open browser with URL, when founded something new

--- parse.js
+++ parse.js
@@ -23,18 +23,18 @@ console.log(`TIMEOUT: ${timeout}`);
 const beep = require('beepbeep');
 const colors = require('colors');
 const https = require('https');
-const openurl = require('openurl');
+const openurl = require('open');
 const bl = require('bl');
-const { JSDOM } = require('jsdom'); 
+const { JSDOM } = require('jsdom');
 let vacList = ["NONE"];
 
 //MAIN FUNCTION
 function parse() {
     // Using https we can take access to both http and https content?
-    https.get(URL, (resp)=>{
+    https.get(URL, (resp) => {
         // SetEncoding for content
         resp.setEncoding('utf8');
-        resp.pipe(bl((err,data)=>{
+        resp.pipe(bl((err, data) => {
             if (err) return console.err(colors.red(err));
             processHTML(data);
         }));
@@ -43,38 +43,38 @@ function parse() {
 
 function processHTML(html) {
     //Use runScripts for getting blocks from page
-    let dom = new JSDOM(html, {runScripts: "outside-only"});
-    
+    let dom = new JSDOM(html, { runScripts: "outside-only" });
+
     let headers = dom.window.eval(`
         [...document.querySelectorAll("${Selector}")].map(el => el.innerHTML.replace(/<[^>]*>/g, ""));
     `);
     processListOfBlocks(headers);
 }
-const substract = function(that, another) {
-    return that.filter(el => !another.some(e=>e===el));
+const substract = function (that, another) {
+    return that.filter(el => !another.some(e => e === el));
 }
 let lastDate;
 let lastVac = "";
 function processListOfBlocks(blocks) {
-    if (blocks.length === 0) return console.log("SOME ERROR, THERE IS NO VACANCIES");
-    if (blocks[0]!==vacList[0]){
-        newVacs = substract(blocks,vacList);
+    if (blocks.length === 0) return console.log(colors.red("SOME ERROR, THERE IS NO VACANCIES"));
+    if (blocks[0] !== vacList[0]) {
+        newVacs = substract(blocks, vacList);
         lastDate = new Date();
         console.log(`${formatTime(lastDate)}${colors.yellow(": NEW VACANCIES!")}\n${colors.green(newVacs.join('\n'))}`);
         beep(2);
         vacList = blocks;
-        openurl.open(URL);
+        openurl(URL, "chrome");
         lastVac = newVacs[0];
     } else {
         const date = new Date();
         const output = colors.gray(colors.italic(`${formatTime(date)}: THERE ISN'T NEW BLOCK VALUES.\nLAST BLOCK at ${formatTime(lastDate)}: ${lastVac}`));
         console.log(output);
 
-    }    
+    }
 }
 function formatTime(date) {
     let [hour, minute] = [date.getHours(), date.getMinutes()];
-    return `${hour<10 ? "0":""}${hour}:${minute<10 ? "0":""}${minute}`
+    return `${hour < 10 ? "0" : ""}${hour}:${minute < 10 ? "0" : ""}${minute}`;
 }
 parse();
 let z = setInterval(parse, timeout);
